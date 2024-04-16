69736117e92721e0c9190e12aa54f1d948454336
Sai Praveen Kondapalli <63105626+saipraveenkondapalli@users.noreply.github.com>
2024-04-16 19:56:25 +0530

    default to light theme

--- frontend2/app/providers.tsx
+++ frontend2/app/providers.tsx
@@ -4,10 +4,11 @@ import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import UserContextProvider from "@/app/UserContext";
 import { ReactQueryClientProvider } from "@/app/ReactQueryProvider";
+import theme from "@/app/theme";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <ReactQueryClientProvider>
         <UserContextProvider>{children}</UserContextProvider>
       </ReactQueryClientProvider>
