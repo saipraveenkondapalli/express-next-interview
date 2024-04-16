df839c46f2df971cc05992feb40a4edfb13c365d
Sai Praveen Kondapalli <63105626+saipraveenkondapalli@users.noreply.github.com>
2024-04-16 11:19:07 +0530

    removed post router

--- backend/src/routes/posts.router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { samplePosts } from "../controllers/post.controller";
-import { checkJwt } from "../middleware/authMiddleware";
-
-const postsRouter = Router();
-
-postsRouter.get("/posts", checkJwt, samplePosts);
-
-export default postsRouter;
